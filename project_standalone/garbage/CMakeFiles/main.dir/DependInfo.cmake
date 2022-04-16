56c8eef102e2399dbcc8a7381b278c962ca6bf70
notabelincoln <abejordandx@gmail.com>
2022-04-16 15:21:18 -0400

    Added cmake file

---
+++ project_standalone/garbage/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/abe/Downloads/github-repositories/ece-417-598-project/project_standalone/garbage/main.cpp" "/home/abe/Downloads/github-repositories/ece-417-598-project/project_standalone/garbage/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
