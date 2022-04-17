47c70d8752200b0dd55bffd2fe9a1f4c9851aa79
notabelincoln <abejordandx@gmail.com>
2022-04-17 18:32:40 -0400

    Added corner detection code

---
+++ attempt4_c++/CMakeFiles/CornerDetection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CornerDetection.dir/CornerDetection.cpp.o"
+  "CornerDetection"
+  "CornerDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CornerDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
