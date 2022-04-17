b3187e9fb9ff4d8f021bf097dd87e01186983456
notabelincoln <abejordandx@gmail.com>
2022-04-17 17:16:56 -0400

    Added code for reading checkerboard, does not work

--- attempt3_python/ece_417_598_project.py
+++ attempt3_python/ece_417_598_project.py
@@ -1,15 +1,56 @@
 #!/usr/bin/python3
 import cv2
+import numpy as np
+
+# Dimensions of checkerboard
+checkerboard_dims = (7, 9)
+critera = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+obj_points = []
+img_points = []
+
+objp = np.zeros((1, checkerboard_dims[0] * checkerboard_dims[1], 3), \
+        np.float32)
+objp[0,:,:2] = np.mgrid[0:checkerboard_dims[0], \
+        0:checkerboard_dims[1]].T.reshape(-1, 2)
+prev_img_shape = None
 
 vid = cv2.VideoCapture(0)
 
 while (True):
     ret, frame = vid.read()
 
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    ret, corners = cv2.findChessboardCorners(gray, checkerboard_dims, \
+            cv2.CALIB_CB_ADAPTIVE_THRESH + cv2.CALIB_CB_FAST_CHECK + \
+            cv2.CALIB_CB_NORMALIZE_IMAGE)
+    
+    if ret == True:
+        obj_points.append(objp)
+        
+        corners2 = cv2.cornerSubpix(gray, corners, (11,11), (-1,-1), criteria)
+
+        img_points.append(corners2)
+
+        frame = cv2.drawChessboardCorners(frame, checkerboard_dims, corners2, ret)
+    
     cv2.imshow('frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+    h, w = frame.shape[:2]
+    
+    '''
+    ret, mtx, dist, rvecs, tvecs = \
+            cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], \
+                None, None)
+
+    print(mtx, dist, rvecs, tvecs)
+    '''
+
 vid.release()
 cv2.destroyAllWindows()
+
