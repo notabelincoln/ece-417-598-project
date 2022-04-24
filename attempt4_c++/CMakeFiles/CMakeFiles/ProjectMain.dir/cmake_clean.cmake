ffc9e08c3ebe0ac8868ab6b20e0d32a20b7be3af
sammk42 <slking232@gmail.com>
2022-04-24 12:16:42 -0400

    Pic

---
+++ attempt4_c++/CMakeFiles/CMakeFiles/ProjectMain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectMain.dir/ProjectMain.cpp.o"
+  "ProjectMain"
+  "ProjectMain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
